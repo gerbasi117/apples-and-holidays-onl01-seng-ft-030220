883ae8e84da34c0129b0ee32986de6ad41765238
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-16 03:05:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,2 +1,6 @@
-def add_supply_to_winter_holidays(holiday_hash, item) holiday_hash[:winter].each do |holiday, decorations| decorations << item end end
+def add_supply_to_winter_holidays(holiday_hash, item) 
+  holiday_hash[:winter].each do |holiday, decorations| 
+    decorations << item 
+    end 
+    end
 
