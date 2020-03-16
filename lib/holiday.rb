cf612bac00bafaccb049165e6efd20b765253a80
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-16 03:12:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,4 +1,14 @@
-
+def second_supply_for_fourth_of_july(holiday_hash)
+  {:winter => {:christmas => ["Lights", "Wreath"], :new_years => ["Party Hats"]},
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]},
+     :fall => {
+       :thanksgiving => ["Turkey"]},
+     :spring => {
+       :memorial_day => ["BBQ"]}}
+   return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
     holiday_hash[:winter].each do |holiday, items|
