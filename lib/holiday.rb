dbbad93e512ea37c5f4a59f8e22d6b7d64271fa3
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-16 03:08:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,68 @@
-def add_supply_to_winter_holidays(holiday_hash, item) 
-  holiday_hash[:winter].each do |holiday, decorations| 
-    decorations << item 
-    end 
-    end
+def second_supply_for_fourth_of_july(holiday_hash)
+  # given that holiday_hash looks like this:
+  # {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
+  # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
+end
 
+def add_supply_to_winter_holidays(holiday_hash, supply)
+    holiday_hash[:winter].each do |holiday, items|
+    items << supply
+    puts items
+    end
+end
+
+
+def add_supply_to_memorial_day(holiday_hash, supply)
+    holiday_hash[:spring].each do |holiday, items|
+    items << supply
+    puts items
+  end
+end
+
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+    holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
+end
+
+def all_winter_holiday_supplies(holiday_hash)
+    array = holiday_hash[:winter].map do |holiday, items|
+    items
+  end
+  array.flatten
+end
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
+      puts "  " + holiday + " " + supplies
+    end
+  end
+end
+
+def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, items|
+      if items.include?("BBQ")
+        holiday
+      end
+    end
+  end.flatten.compact
+end
